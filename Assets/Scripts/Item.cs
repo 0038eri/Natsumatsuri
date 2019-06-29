6f448700b1a507321d81a2d0cfa13f7d73e864aa
Eri Osawa <osawaeri@users.noreply.github.com>
2019-06-29 12:13:36 +0900

    6/29

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -6,36 +6,47 @@ using UnityEngine.UI;
 public class Item : MonoBehaviour
 {
 
-  private Text text;
+  private Rigidbody rigidbody_;
 
   private GameObject itemManager;
   private ItemManager item_manager;
   private Rigidbody im_rigidbody;
 
-  private GameObject RightHandAnchor;
-
   private int steps = 0;
+
+  private Text text;
 
   void Start()
   {
+    rigidbody_ = GetComponent<Rigidbody>();
+
     itemManager = GameObject.Find("ItemManager");
     item_manager = itemManager.GetComponent<ItemManager>();
-    RightHandAnchor = GameObject.Find("RightHandAnchor");
-    im_rigidbody = RightHandAnchor.GetComponent<Rigidbody>();
+    im_rigidbody = itemManager.GetComponent<Rigidbody>();
+
     text = GameObject.Find("DebugText").GetComponent<Text>();
   }
 
   void Update()
   {
-    text.text = steps.ToString();
+    text.text = im_rigidbody.velocity.ToString();
+
     if (steps == 2)
     {
       transform.position = itemManager.transform.position;
+      transform.rotation = itemManager.transform.rotation;
     }
     else if (steps == 3)
     {
-      this.GetComponent<Rigidbody>().velocity = im_rigidbody.velocity;
-      this.GetComponent<Rigidbody>().angularVelocity = im_rigidbody.angularVelocity;
+      rigidbody_.velocity = im_rigidbody.velocity;
+      rigidbody_.angularVelocity = im_rigidbody.angularVelocity;
+    }
+    else if (steps == 4)
+    {
+      if (transform.position.y < -10)
+      {
+        Destroy(this.gameObject);
+      }
     }
   }
 
--- Assets/Scripts/ItemManager.cs
+++ Assets/Scripts/ItemManager.cs
@@ -1,71 +1,85 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class ItemManager : MonoBehaviour
 {
-
-  // public Text debugText;
-
   public GameObject item_prefab;
 
   private GameObject item;
   private Item item_;
 
-  public int steps = 0;
+  private int steps = 0;
   private bool itemFlag = false;
   private bool triggerFlag = false;
 
 
   void Update()
   {
-
-    if (steps == 0)
+    if (OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger))
     {
       if (triggerFlag == false)
       {
-        if (OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger))
-        {
-          steps++;
-          triggerFlag = true;
-        }
-      }
-    }
-    else if (steps == 2)
-    {
-      if (OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger))
-      {
         steps++;
       }
+      triggerFlag = true;
     }
 
     if (steps == 1)
     {
-      item = Instantiate(item_prefab, transform.position, transform.rotation);
-      item_ = item.GetComponent<Item>();
-      itemFlag = true;
-      steps = 2;
+      if (itemFlag == false)
+      {
+        item = Instantiate(item_prefab, transform.position, transform.rotation);
+        item_ = item.GetComponent<Item>();
+        itemFlag = true;
+      }
+    }
+    else if (steps == 2)
+    {
+      if (triggerFlag == true)
+      {
+        if (OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger))
+        {
+          steps++;
+          item_.receiveSteps();
+        }
+      }
     }
     else if (steps == 3)
     {
       // 投げる
-      item_.receiveSteps();
-      steps = 4;
     }
     else if (steps == 4)
     {
-      itemFlag = false;
+      // 待機
+    }
+    else if (steps == 5)
+    {
       steps = 0;
+      itemFlag = false;
       triggerFlag = false;
     }
+  }
 
-    if (itemFlag == true)
+  void FixedUpdate()
+  {
+    if (steps == 1)
     {
+      steps = 2;
       item_.receiveSteps();
     }
-
-    // debugText.text = steps.ToString();
+    else if (steps == 3)
+    {
+      steps = 4;
+      item_.receiveSteps();
+    }
+    else if (steps == 4)
+    {
+      if (item == null)
+      {
+        steps = 5;
+      }
+    }
   }
 
   public int sendSteps()
